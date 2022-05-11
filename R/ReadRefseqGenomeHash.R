f796c1a033752c15b0f51c820c69b7eb7a70b140
selkamand <sam.elkamand@gmail.com>
2022-05-11 11:51:08 +1000

    inital commit

---
+++ R/ReadRefseqGenomeHash.R
@@ -0,0 +1,249 @@
+
+
+#' Cache refseq contents
+#'
+#' Download a file that lists metadata for all refseq entries (including ftp path).
+#' We will use this later to download assemblies.
+#'
+#' @return refseq dataframe describing available assemblies
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' load_refseq_data_frame_from_ftp()
+#' }
+load_refseq_data_frame_from_ftp <- function(){
+  refseq_data_frame = read.csv("https://ftp.ncbi.nlm.nih.gov/genomes/refseq/assembly_summary_refseq.txt", sep = "\t", header=TRUE, skip = 1, check.names = FALSE)
+  names(refseq_data_frame) <- sub(pattern = "# ?", replacement = "", x = names(refseq_data_frame))
+  # assembly_accession
+
+  required_columns = c("species_taxid", "refseq_category", "organism_name", "assembly_level", "assembly_accession", "version_status")
+  utilitybeltassertions::assert_names_include(refseq_data_frame, expected_names = required_columns)
+  assertthat::assert_that(nrow(refseq_dataframe) > 0, msg = utilitybeltassertions::fmterror("Refseq Dataframe has 0 rows. Somethings probably wrong with the ftp download link. Please ensure you're using the latest version of this package, and if the problem persists, open a github issue so package maintaner can fix this"))
+
+  # Guess best reference
+  refseq_data_frame = refseq_data_frame %>%
+    dplyr::group_by(taxid) %>%
+    dplyr::mutate(
+      ref_score = 0 +
+        (refseq_category=="reference genome") * 10^5 +
+        (refseq_category=="representative genome") * 10^4 +
+        (genome_rep=="Full") * 10^3 +
+        (assembly_level == "Complete Genome") * 4*10^2 +
+      (assembly_level == "Chromosome") * 3*10^2 +
+      (assembly_level == "Scaffold") * 2*10^2 +
+      (assembly_level == "Contig") * 1*10^2,
+      best_ref = ref_score == max(ref_score)
+    ) %>%
+    dplyr::ungroup()
+
+  return(refseq_data_frame)
+}
+
+write_refseq_dataframe <- function(refseq_data_frame){
+  outfile = paste0(system.file(package='utilitybeltrefseq'), "/assembly_summary_refseq.txt", collapse = "")
+
+  message("Writing table to refseq table to file: [", outfile, "]")
+    write.table(
+      x = refseq_data_frame,
+      file = outfile,
+      row.names = FALSE,
+      sep = "\t"
+    )
+}
+
+#' Load data from cache
+#'
+#' Loads a dataframe describing reference genomes and their ftp links
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' refseq_data_frame = load_refseq_data_frame_from_cache()
+load_refseq_data_frame_from_cache <- function(){
+  expected_filepath = paste0(system.file(package='utilitybeltrefseq'), "/assembly_summary_refseq.txt", collapse = "")
+  assertthat::assert_that(file.exists(expected_filepath), msg = paste0("Could not find a cached refseq dataframe at [",expected_filepath,"]. Please run write_refseq_dataframe(load_refseq_data_frame_from_ftp())"))
+
+  #browser()
+  refseq_data_frame = data.table::fread(expected_filepath, sep = "\t", check.names = FALSE)
+  refseq_data_frame
+  required_columns = c("taxid", "species_taxid", "refseq_category", "organism_name", "assembly_level", "assembly_accession", "version_status", "ref_score", "seq_rel_date")
+  #browser()
+  utilitybeltassertions::assert_names_include(refseq_data_frame, expected_names = required_columns)
+  return(refseq_data_frame)
+}
+
+
+
+#' Best Assembly
+#'
+#' @param taxid_of_interest taxonomyID (int)
+#' @param intraspecific_filter e.g. 'strain=Raji'. For options look at the infraspecific_name column of the refseq_data_frame (string)
+#' @param break_ties_based_on_newest_sequence_added if we cant decide which is the best ref to use because they;re so similar - just pick the most recently uploaded sequence
+#' @param refseq_data_frame from \strong{load_refseq_data_frame_from_cache()}
+#'
+#' @return
+#' @export
+#'
+choose_best_assembly <- function(refseq_data_frame = load_refseq_data_frame_from_cache(), taxid_of_interest, intraspecific_filter = NA, break_ties_based_on_newest_sequence_added = TRUE, return_accession_only = TRUE){
+
+  # is species in refseq database
+  assertthat::assert_that(taxid_of_interest %in% refseq_data_frame[["species_taxid"]],
+                          msg = utilitybeltassertions::fmterror("Can't find taxid [", taxid_of_interest, "]", " in refseq database. Are you sure this is a species-level taxid with a refseq assembly?"))
+
+
+  best_assemblies = refseq_data_frame %>%
+    dplyr::filter(species_taxid == taxid_of_interest) %>%
+    dplyr::slice_max(ref_score, with_ties = TRUE)
+
+  if(!is.na(intraspecific_filter)){
+    message("running through intraspecific filter: ", intraspecific_filter)
+    best_assemblies <- best_assemblies %>%
+      dplyr::filter(infraspecific_name == intraspecific_filter)
+    assertthat::assert_that(nrow(best_assemblies) > 0, msg = utilitybeltassertions::fmterror("The addition of intraspecific species filter: [",intraspecific_filter,"] does not contain any of the species level best assemblies"))
+
+  assertthat::assert_that(
+    best_assemblies[["ref_score"]] > 0,
+    msg = utilitybeltassertions::fmterror("Couldn't find any decent assemblies to use: perhaps try to manually find a good refseq assembly accession?"))
+    }
+
+  score_of_tophit = max(best_assemblies[["ref_score"]])
+  if (nrow(best_assemblies) > 1){
+    num_of_tied_assemblies = nrow(best_assemblies)
+
+    if(break_ties_based_on_newest_sequence_added){
+      best_assemblies_final=dplyr::slice_max(best_assemblies, seq_rel_date)
+      message("Multile (", num_of_tied_assemblies,")", " best hits with score = ",score_of_tophit,". We will just return the the most recently added assembly with this quality\n")
+      prettyprint_single_row_df(best_assemblies_final, title = "Chosen Assembly: ")
+      if(return_accession_only) return(best_assemblies_final[["assembly_accession"]]) else return(dplyr::tibble(best_assemblies_final))
+    }
+    else{
+      message("Multile (", num_of_tied_assemblies,")", " best hits with score = ",score_of_tophit, ". Please choose one manually or if you haven't already - add an intraspecific filter and try again")
+      best_assemblies_final=dplyr::tibble(best_assemblies)
+      if(return_accession_only) return(best_assemblies_final[["assembly_accession"]]) else return(dplyr::tibble(best_assemblies_final))
+    }
+
+  }
+
+  if(return_accession_only) return(best_assemblies[["assembly_accession"]]) else return(dplyr::tibble(best_assemblies))
+}
+
+#' Pretty Print a Single Refseq Entry
+#'
+#' @param single_row_of_tabular_data any dataframe with 1 row. Doesn't require any particular column_names / row typing. (data.frame)
+#' @return NULL
+prettyprint_single_row_df <- function(single_row_of_tabular_data, title = NULL){
+  assertthat::assert_that(nrow(single_row_of_tabular_data) == 1, msg = utilitybeltassertions::fmterror("Function `prettyprint_assembly_entry` was designed to work with single row dataframes. Input dataframe has ", nrow(single_row_of_tabular_data), " entries"))
+  names = names(single_row_of_tabular_data)
+  values = as.character(single_row_of_tabular_data)
+
+  prettyprint_format = paste("\t", utilitybeltassertions::fmtbold(names), "\t",values, "\n")
+
+  if(!is.null(title)){
+    message(utilitybeltassertions::fmtbold(title))
+  }
+
+  message(prettyprint_format)
+
+  return(invisible(NULL))
+}
+
+#' Download reference genome
+#'
+#' Actually download the genome using aria2c
+#'
+#' @inheritParams choose_best_assembly
+#' @param target_assembly_accession accession of target assembly
+#' @param outfile_dir directory to save assembly fasta file into
+#'
+#' @return Run for its side effects
+#' @export
+download_assembly <- function(refseq_data_frame = load_refseq_data_frame_from_cache(), target_assembly_accession){
+
+  #browser()
+  assertthat::assert_that(target_assembly_accession %in% refseq_data_frame[["assembly_accession"]])
+
+  refseq_data_frame = refseq_data_frame %>%
+    dplyr::filter(assembly_accession %in% target_assembly_accession)
+
+  assertthat::assert_that(nrow(refseq_data_frame) == 1)
+
+  sequence_file_name = basename(refseq_data_frame[["ftp_path"]])
+  ftp_root = refseq_data_frame[["ftp_path"]]
+  assembly_fasta_path = paste0(ftp_root, "/", sequence_file_name, "_genomic.fna.gz")
+  #browser()
+  message("downloading assembly:\n\t[", target_assembly_accession, "]\n\nfrom the RefSeq ftp link \n\t[", assembly_fasta_path, "]")
+
+  download.file(assembly_fasta_path, destfile =  basename(assembly_fasta_path))
+  # res=system(paste0("aria2c ", assembly_fasta_path))
+  # if (res != 0)
+  #   message("Download failed")
+  # else
+  #   message("download complete")
+
+  #sys.calls(
+}
+
+
+download_assembly_aria2c <- function(refseq_data_frame = load_refseq_data_frame_from_cache(), target_assembly_accession){
+
+  utilitybeltassertions::assert_program_exists_in_path("aria2c")
+  #browser()
+  assertthat::assert_that(target_assembly_accession %in% refseq_data_frame[["assembly_accession"]])
+
+  refseq_data_frame = refseq_data_frame %>%
+    dplyr::filter(assembly_accession %in% target_assembly_accession)
+
+  assertthat::assert_that(nrow(refseq_data_frame) == 1)
+
+  sequence_file_name = basename(refseq_data_frame[["ftp_path"]])
+  ftp_root = refseq_data_frame[["ftp_path"]]
+  assembly_fasta_path = paste0(ftp_root, "/", sequence_file_name, "_genomic.fna.gz")
+#browser()
+  message("downloading assembly:\n\t[", target_assembly_accession, "]\n\nfrom the RefSeq ftp link \n\t[", assembly_fasta_path, "]")
+
+  res=system(paste0("aria2c ", assembly_fasta_path))
+  if (res != 0)
+    message("Download failed")
+  else
+    message("download complete")
+
+  #sys.calls(
+}
+
+#' Download_best_assembly
+#'
+#' @inheritDotParams download_assembly
+#' @inheritParams choose_best_assembly
+#' @export
+#'
+download_best_assembly <- function(taxid_of_interest, ...){
+  best_asssembly=target_assembly_accession = choose_best_assembly(taxid_of_interest = taxid_of_interest, return_accession_only = TRUE)
+  message("Attempting to download the best assembly for taxid ", taxid_of_interest, " (", best_asssembly, ")")
+  download_assembly(target_assembly_accession = best_asssembly, ...)
+}
+
+download_annotations <- function(refseq_data_frame = load_refseq_data_frame_from_cache(), target_assembly_accession, outfile_dir = "."){
+
+  utilitybeltassertions::assert_program_exists_in_path("aria2c")
+  #browser()
+  assertthat::assert_that(target_assembly_accession %in% refseq_data_frame[["assembly_accession"]])
+
+  refseq_data_frame = refseq_data_frame %>%
+    dplyr::filter(assembly_accession %in% target_assembly_accession)
+
+  assertthat::assert_that(nrow(refseq_data_frame) == 1)
+
+  sequence_file_name = basename(refseq_data_frame[["ftp_path"]])
+  ftp_root = refseq_data_frame[["ftp_path"]]
+  assembly_fasta_path = paste0(ftp_root, "/", sequence_file_name, "_genomic.gtf.gz")
+  #browser()
+  message("downloading assembly:\n\t[", target_assembly_accession, "]\n\nfrom the RefSeq ftp link \n\t[", assembly_fasta_path, "]")
+
+  res=system(paste0("aria2c ", assembly_fasta_path))
+  if (res != 0)
+   message("Download failed")
+  else
+    message("download complete")
+}
---
+++ README.md
@@ -0,0 +1,135 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# utilitybeltrefseq
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+
+<!-- badges: end -->
+
+The goal of utilitybeltrefseq is to make it easy to find and download
+the best refseq assemblies available for any given species. All you need
+to know is the species [NCBI taxonomy
+ID](https://www.ncbi.nlm.nih.gov/taxonomy)
+
+## Installation
+
+You can install the development version of utilitybeltrefseq from
+[GitHub](https://github.com/selkamand/utilitybeltrefseq) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("selkamand/utilitybeltrefseq")
+```
+
+## Example
+
+Say you want to find the best reference genome for *Escherichia coli*.
+
+First we download a list of available assemblies from refseq.
+
+``` r
+library(utilitybeltrefseq)
+load_refseq_data_frame_from_ftp()
+#> # A tibble: 259,672 × 25
+#>    assembly_accession bioproject  biosample    wgs_master refseq_category  taxid
+#>    <chr>              <chr>       <chr>        <chr>      <chr>            <int>
+#>  1 GCF_000001215.4    PRJNA164    "SAMN028037… ""         reference geno…   7227
+#>  2 GCF_000001405.40   PRJNA168    ""           ""         reference geno…   9606
+#>  3 GCF_000001635.27   PRJNA169    ""           ""         reference geno…  10090
+#>  4 GCF_000001735.4    PRJNA116    "SAMN030814… ""         reference geno…   3702
+#>  5 GCF_000001905.1    PRJNA70973  "SAMN029536… "AAGU0000… representative…   9785
+#>  6 GCF_000001985.1    PRJNA32665  "SAMN029536… "ABAR0000… representative… 441960
+#>  7 GCF_000002035.6    PRJNA13922  "SAMN069301… ""         reference geno…   7955
+#>  8 GCF_000002075.1    PRJNA209509 "SAMN029536… "AASC0000… representative…   6500
+#>  9 GCF_000002235.5    PRJNA13728  "SAMN008294… "AAGJ0000… representative…   7668
+#> 10 GCF_000002285.5    PRJNA12384  "SAMN029536… "AAEX0000… na                9615
+#> # … with 259,662 more rows, and 19 more variables: species_taxid <int>,
+#> #   organism_name <chr>, infraspecific_name <chr>, isolate <chr>,
+#> #   version_status <chr>, assembly_level <chr>, release_type <chr>,
+#> #   genome_rep <chr>, seq_rel_date <chr>, asm_name <chr>, submitter <chr>,
+#> #   gbrs_paired_asm <chr>, paired_asm_comp <chr>, ftp_path <chr>,
+#> #   excluded_from_refseq <chr>, relation_to_type_material <chr>,
+#> #   asm_not_live_date <chr>, ref_score <dbl>, best_ref <lgl>
+```
+
+For best results, run the above command every couple of months to stay
+up to date with whats currently in refseq.
+
+Next, we need to find the NCBI taxonomy ID of our species of interest.
+We can find that [here](https://www.ncbi.nlm.nih.gov/taxonomy) (taxid:
+562). We could also have used the **taxize** package if we wanted to
+stay within R.
+
+Now that we have the species level taxid, we can run
+`choose_best_assembly`
+
+``` r
+choose_best_assembly(taxid_of_interest = 562)
+#> Multile (2) best hits with score = 101400. We will just return the the most recently added assembly with this quality
+#> Chosen Assembly:
+#>   assembly_accession      GCF_000008865.2 
+#>   bioproject      PRJNA57781 
+#>   biosample   SAMN01911278 
+#>   wgs_master       
+#>   refseq_category     reference genome 
+#>   taxid   386585 
+#>   species_taxid   562 
+#>   organism_name   Escherichia coli O157:H7 str. Sakai 
+#>   infraspecific_name      strain=Sakai substr. RIMD 0509952 
+#>   isolate      
+#>   version_status      latest 
+#>   assembly_level      Complete Genome 
+#>   release_type    Major 
+#>   genome_rep      Full 
+#>   seq_rel_date    2018/06/08 
+#>   asm_name    ASM886v2 
+#>   submitter   GIRC 
+#>   gbrs_paired_asm     GCA_000008865.2 
+#>   paired_asm_comp     identical 
+#>   ftp_path    https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/008/865/GCF_000008865.2_ASM886v2 
+#>   excluded_from_refseq     
+#>   relation_to_type_material    
+#>   asm_not_live_date   na 
+#>   ref_score   101400 
+#>   best_ref    TRUE
+#> [1] "GCF_000008865.2"
+```
+
+Note that we get a warning that there are multiple ‘best’ assemblies. If
+there are multiple strains in a species with complete assemblies - it
+his hard to know which to return. We choose the most recently added
+assembly. To return all of the high quality assemblies you can run:
+
+``` r
+ choose_best_assembly(
+   taxid_of_interest = 562, 
+   break_ties_based_on_newest_sequence_added = FALSE,
+   return_accession_only = TRUE # set this to false to get more info about each assembly
+   )
+#> Multile (2) best hits with score = 101400. Please choose one manually or if you haven't already - add an intraspecific filter and try again
+#> [1] "GCF_000005845.2" "GCF_000008865.2"
+```
+
+Keep an eye on the intraspecific name column. Often can use this to
+choose which strain you’re after. See `?choose_best_assembly` for
+details.
+
+Once we have the assembly accession we’re interested in, we can download
+it:
+
+``` r
+ download_assembly(
+   target_assembly_accession = GCF_000005845.2
+   )
+```
+
+If we just want to quickly download a ref-genome of our species of
+interest we can just run
+
+``` r
+download_best_assembly(taxid_of_interest = 562)
+```
